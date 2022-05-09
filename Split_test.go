ae96ada54f9fd943e55333e85ab0c73aa8932598
syz <360579418@qq.com>
2022-05-09 20:24:21 +0800

    git commit Split.go

--- Split_test.go
+++ Split_test.go
@@ -6,17 +6,23 @@ import (
 )
 
 func TestSplit(t *testing.T) {
-	got := Split("a:b:c", ":")
-	want := []string{"a", "b", "c"}
-	if !reflect.DeepEqual(want, got) {
-		t.Errorf("Excepted:%v,got:%v", want, got)
+	//定义一个测试用例类型
+	type test struct {
+		input string
+		sep   string
+		want  []string
 	}
-}
-
-func TestMoreSplit(t *testing.T) {
-	got := Split("abcd", "bc")
-	want := []string{"a", "d"}
-	if !reflect.DeepEqual(want, got) {
-		t.Errorf("Excepted:%v,got:%v", want, got)
+	//定义一个存储测试用例的切片
+	tests := []test{
+		{input: "a:b:c", sep: ":", want: []string{"a", "b", "c"}},
+		{input: "a:b:c", sep: ",", want: []string{"a:b:c"}},
+		{input: "abcd", sep: "bc", want: []string{"a", "d"}},
+		{input: "这是一次测试", sep: "一", want: []string{"这是", "次测试"}},
+	}
+	for _, tc := range tests {
+		got := Split(tc.input, tc.sep)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("Excepted:%v,got:%v", tc.want, got)
+		}
 	}
 }
